0d77747b48ccced72f52aaf27a09e9ef5a089c74
Naime Kaymaz <GITHUB_MAIL_ADRESIN>
2026-03-12 13:35:06 +0300

    remove database for railway

--- Program.cs
+++ Program.cs
@@ -8,16 +8,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connStr = builder.Configuration.GetConnectionString("Default");
-
-if (!string.IsNullOrWhiteSpace(connStr))
-{
-    builder.Services.AddDbContext<AppDbContext>(opt =>
-    {
-        opt.UseMySql(connStr, ServerVersion.AutoDetect(connStr));
-    });
-}
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -32,12 +22,5 @@ app.UseStaticFiles();
 // app.UseMiddleware<ApiKeyMiddleware>();
 
 app.MapControllers();
-
-if (!string.IsNullOrWhiteSpace(connStr))
-{
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-}
 
 app.Run("http://0.0.0.0:8080");
